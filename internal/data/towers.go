de3e6d88b523ca9a8bd4ae272eedd1b837ad7341
spargonaut <chiapet@spargonaut.com>
2023-11-18 22:12:40 -0800

    create the grow tower struct and return a hardcoded one from the get handler

---
+++ internal/data/towers.go
@@ -0,0 +1,17 @@
+package data
+
+import (
+	"time"
+)
+
+type GrowTower struct {
+	ID           int64     `json:"id"`
+	CreatedAt    time.Time `json:"-"`
+	Name         string    `json:"name,omitempty"`
+	Type         string    `json:"type"`
+	TargetPhLow  float32   `json:"target_ph_low,omitempty"`
+	TargetPhHigh float32   `json:"target_ph_high,omitempty"`
+	TargetECLow  float32   `json:"target_ec_low,omitempty"`
+	TargetECHigh float32   `json:"target_ec_high,omitempty"`
+	Version      int32     `json:"-"`
+}
--- services/tower-profile-api/handlers.go
+++ services/tower-profile-api/handlers.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/spargonaut/plant_info_service/internal/data"
 	"net/http"
+	"time"
 )
 
 func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
@@ -34,13 +36,42 @@ func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type envelope map[string]any
+
 func (app *application) getGrowTowersHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		return
 	}
 
-	fmt.Fprintln(w, "Display a list of the grow towers")
+	towers := []data.GrowTower{
+		{
+			ID:           23,
+			CreatedAt:    time.Now(),
+			Name:         "Lettuce Tower",
+			Type:         "FarmStand",
+			TargetPhLow:  6,
+			TargetPhHigh: 7.5,
+			TargetECLow:  0.8,
+			TargetECHigh: 1.2,
+			Version:      1,
+		},
+	}
+
+	js, err := json.MarshalIndent(envelope{"towers": towers}, "", "\t")
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(js)
+	if err != nil {
+		fmt.Println("Error writing the GET growtowers response.")
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
 
 func (app *application) createGrowTowerHandler(w http.ResponseWriter, r *http.Request) {
